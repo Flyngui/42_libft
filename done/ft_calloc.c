63e3414bc19dbf712011f92ccfe62488b809703b
Guilherme Lopes <guiferre@student.42lisboa.com>
2024-11-05 00:12:40 +0000

    calloc bzero

--- done/ft_calloc.c
+++ done/ft_calloc.c
@@ -1,33 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: guiferre <guiferre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/04 18:54:39 by guiferre          #+#    #+#             */
-/*   Updated: 2024/11/04 22:30:00 by guiferre         ###   ########.fr       */
+/*   Created: 2024/11/04 18:29:03 by guiferre          #+#    #+#             */
+/*   Updated: 2024/11/04 18:49:41 by guiferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
+#include <stdlib.h>
 
-void	ft_bzero(void *s, int n)
+int	ft_calloc(int n, int size)
 {
-	int		i;
+	int	i;
+	int	*arr;
 
 	i = 0;
+	if ((n * size) > 2147483647)
+		return (NULL);
+	arr = malloc(size * n);
+	if (arr == NULL)
+		return (NULL);
 	while (i < n)
 	{
-		((char *) s)[i] = '\0';
+		arr[i] = 0;
 		i++;
 	}
-}
-
-int	main(void)
-{
-	int i = 5;
-	char *arr = "Hello World";
-
-	ft_bzero(arr, i);
-	printf("%s", arr);
+	return (arr);
 }
