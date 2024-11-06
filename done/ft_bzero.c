4b1fdcaa2b9642d7fabda6993d0a6df799dc3782
Guilherme Lopes <guiferre@student.42lisboa.com>
2024-11-06 00:08:05 +0000

    bzero memcmp memset

--- done/ft_bzero.c
+++ done/ft_bzero.c
@@ -6,14 +6,13 @@
 /*   By: guiferre <guiferre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 18:54:39 by guiferre          #+#    #+#             */
-/*   Updated: 2024/11/04 22:30:00 by guiferre         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:15:48 by guiferre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
 
 void	ft_bzero(void *s, int n)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (i < n)
@@ -21,13 +20,4 @@ void	ft_bzero(void *s, int n)
 		((char *) s)[i] = '\0';
 		i++;
 	}
-}
-
-int	main(void)
-{
-	int i = 5;
-	char *arr = "Hello World";
-
-	ft_bzero(arr, i);
-	printf("%s", arr);
 }
